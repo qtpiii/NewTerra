10763340a7fb281af08a9069c52f417af862fbce
hootis <81767621+hoootis@users.noreply.github.com>
2025-01-25 23:23:32 -0800

    not working yet !! but its a start

--- src/PlayerHooks.cs
+++ src/PlayerHooks.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using UnityEngine;
 
 namespace NewTerra
@@ -15,16 +18,74 @@ namespace NewTerra
 			{
 				On.PlayerGraphics.ctor += PlayerGraphics_ctor;
 				On.PlayerGraphics.DrawSprites += PlayerGraphics_DrawSprites;
+				On.PlayerGraphics.Update += PlayerGraphicsOnUpdate;
 
 				On.Creature.Violence += Creature_Violence;
+				On.Creature.ctor += CreatureOnctor;
 				On.Player.Update += Player_Update;
 				On.Player.ctor += Player_ctor;
 				On.Player.TerrainImpact += Player_TerrainImpact;
+				IL.Player.GraphicsModuleUpdated += PlayerOnGraphicsModuleUpdated;
 			}
 			catch (Exception ex)
 			{
 				Plugin.logger.LogFatal(ex);
 			}
+		}
+
+		private void PlayerGraphicsOnUpdate(On.PlayerGraphics.orig_Update orig, PlayerGraphics self)
+		{
+			orig(self);
+
+			if (self.player.SlugCatClass.value == Plugin.TARDIGOATED_ID)
+			{
+				for (int i = 0; i < 2; i++)
+				{
+					self.hands[i].retract = false;//self.player.grasps[i + 2] == null || self.player.grasps[i] == null;
+					self.hands[i].retractCounter = 0;
+					self.hands[i].mode = Limb.Mode.HuntRelativePosition;
+					self.hands[i + 2].pos = self.hands[i].pos;
+					self.hands[i + 2].pos -= new Vector2(0, 10);
+				}
+			}
+		}
+
+		private void PlayerOnGraphicsModuleUpdated(ILContext il)
+		{
+			ILCursor c = new(il);
+			
+			/*
+			for (int i = 0; i < 2; i++)
+			{
+				c.GotoNext(
+					x => x.MatchLdfld<PlayerGraphics>(nameof(PlayerGraphics.hands)),
+					x => x.MatchLdloc(0),
+					x => x.MatchLdelemRef()
+				);
+				Debug.Log(c);
+				c.Index += 2;
+				Debug.Log(c);
+				c.EmitDelegate((int j) =>
+				{
+					Debug.Log("TEST: " + j);
+					Debug.Log(Mathf.Clamp(j, 0, 1));
+					return Mathf.Clamp(j, 0, 1);
+				});
+				Debug.Log(c);
+			}
+			*/
+			
+			c.GotoNext(
+				x => x.MatchLdloc(0),
+				x => x.MatchLdcI4(2),
+				x => x.MatchBlt(out _)
+			);
+			c.Index += 2;
+			c.Emit(OpCodes.Ldarg_0);
+			c.EmitDelegate((int grasps, Player player) =>
+			{
+				return player.grasps.Length;
+			});
 		}
 
 		#region graphics
@@ -106,13 +167,21 @@ namespace NewTerra
 				bp.RemoveAll(x => x is TailSegment);
 				bp.AddRange(self.tail);
 				self.bodyParts = bp.ToArray();
-			}
 
+				self.hands = new SlugcatHand[4];
+				var list = self.bodyParts.ToList();
+				for (int j = 0; j < 4; j++)
+				{
+					self.hands[j] = new SlugcatHand(self, self.owner.bodyChunks[0], j, 3f, 0.8f, 1f);
+					list.Add(self.hands[j]);
+				}
+				self.bodyParts = list.ToArray();
+			}
 		}
 
 		#endregion
 
-		#region damage resistance
+		#region gameplay
 
 		private void Player_TerrainImpact(On.Player.orig_TerrainImpact orig, Player self, int chunk, RWCustom.IntVector2 direction, float speed, bool firstContact)
 		{
@@ -140,9 +209,52 @@ namespace NewTerra
 		private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
 		{
 			orig(self, eu);
-			if (self != null && self.SlugCatClass.value == Plugin.TARDIGOATED_ID)
+			if (self.SlugCatClass.value == Plugin.TARDIGOATED_ID)
 			{
 				self.Hypothermia -= 0.75f * self.HypothermiaGain;
+			}
+			
+			/*
+			if (self.grasps[0] != null && self.grasps[1] != null && (self.grasps[2] == null || self.grasps[3] == null))
+			{
+				foreach (var physObj in self.room.physicalObjects[2])
+				{
+					if (self.grasps[2] == null)
+					{
+						self.SlugcatGrab(physObj, 2);
+					}
+					else if (self.grasps[3] == null)
+					{
+						self.SlugcatGrab(physObj, 3);
+					}
+				}
+			}
+			*/
+
+			if (self.grasps[0] != null && self.grasps[1] != null && self.input[0].pckp != self.input[1].pckp)
+			{
+				foreach (var physObj in self.room.physicalObjects[2])
+				{
+					if (self.grasps[2] == null && Vector2.Distance(physObj.firstChunk.pos, self.firstChunk.pos) < 30)
+					{
+						self.SlugcatGrab(physObj, 2);
+					}
+				}
+			}
+
+			if (self.grasps[2] != null)
+			{
+				self.grasps[2].grabbedChunk.MoveFromOutsideMyUpdate(eu, self.firstChunk.pos + new Vector2(-10, 0));
+
+				if (self.grasps[2].grabbed is Weapon weapon)
+				{
+					//weapon.ChangeMode(Weapon.Mode.Carried);
+				}
+				
+				if (self.grasps[2].grabbed is Spear spear)
+				{
+					//spear.setRotation = Vector2.up;
+				}
 			}
 		}
 
@@ -168,6 +280,21 @@ namespace NewTerra
 				}
 			}
 			orig(self, source, directionAndMomentum, hitChunk, hitAppendage, type, damage, stunBonus);
+		}
+		
+		// for changing the length of the grasps array
+		private void CreatureOnctor(On.Creature.orig_ctor orig, Creature self, AbstractCreature abstractcreature, World world)
+		{
+			orig(self, abstractcreature, world);
+			
+			if (self is Player player)
+			{
+				player.GetInitialSlugcatClass();
+				if (player.SlugCatClass.value == Plugin.TARDIGOATED_ID)
+				{
+					self.grasps = new Creature.Grasp[4];
+				}
+			}
 		}
 
 		#endregion
