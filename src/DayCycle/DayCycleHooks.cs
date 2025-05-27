14babe5b04596597721a95f78202a9e584758ed3
hootis <81767621+hoootis@users.noreply.github.com>
2025-05-27 10:59:34 -0700

    gay

--- src/DayCycle/DayCycleHooks.cs
+++ src/DayCycle/DayCycleHooks.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 using On.DevInterface;
 using UnityEngine;
 using DevUI = DevInterface.DevUI;
@@ -26,6 +30,92 @@ public class DayCycleHooks
 		On.RoomSettings.ctor_string_Region_bool_bool_Name += (orig, self, name, region, template, firstTemplate, playerChar) => { orig(self, name, region, template, firstTemplate, playerChar); AttachCWT(self); };
 		On.RoomSettings.ctor_string_Region_bool_bool_Timeline_RainWorldGame += (orig, self, name, region, template, firstTemplate, point, game) => { orig(self, name, region, template, firstTemplate, point, game); AttachCWT(self); };
 		On.RoomSettings.ctor_Room_string_Region_bool_bool_Timeline_RainWorldGame += (orig, self, room, name, region, template, firstTemplate, point, game) => { orig(self, room, name, region, template, firstTemplate, point, game); AttachCWT(self); };
+		
+		// saving and loading of ext data
+		On.RoomSettings.Save_string_bool += RoomSettingsOnSave_string_bool;
+		On.RoomSettings.Load_Timeline += RoomSettingsOnLoad_Timeline;
+	}
+	
+	private static void RoomSettingsOnSave_string_bool(On.RoomSettings.orig_Save_string_bool orig, RoomSettings self, string path, bool saveAsTemplate)
+	{
+		orig(self, path, saveAsTemplate);
+		
+		var ext = DayCycleExtensions.extensionTable.GetOrCreateValue(self);
+		
+		string originalText = File.ReadAllText(path);
+		using StreamWriter writer = File.CreateText(path);
+		writer.Write(originalText);
+		
+		if (ext.time != 0f)
+		{
+			writer.WriteLine($"Time: {ext.time}");
+		}
+
+		StringBuilder palettes = new();
+		for (int i = 0; i < ext.dayCyclePalettes.GetLength(1); i++)
+		{
+			if (ext.dayCyclePalettes[0, i] != null)
+			{
+				palettes.Append($"main_{i},{ext.dayCyclePalettes[0, i]};");
+			}
+		}
+		for (int i = 0; i < ext.dayCyclePalettes.GetLength(1); i++)
+		{
+			if (ext.dayCyclePalettes[0, i] != null)
+			{
+				palettes.Append($"fade_{i},{ext.dayCyclePalettes[0, i]};");
+			}
+		}
+		writer.WriteLine($"DayCyclePalettes: {palettes}");
+	}
+
+	private static bool RoomSettingsOnLoad_Timeline(On.RoomSettings.orig_Load_Timeline orig, RoomSettings self, SlugcatStats.Timeline timelinepoint)
+	{
+		bool origReturn = orig(self, timelinepoint);
+
+		if (!origReturn) // if the original method failed for whatever reason
+		{
+			return false;
+		}
+
+		var ext = DayCycleExtensions.extensionTable.GetOrCreateValue(self);
+		
+		string[] rawFile = File.ReadAllLines(self.filePath);
+		List<string[]> keyValuePairs = new List<string[]>();
+		for (int i = 0; i < rawFile.Length; i++)
+		{
+			string[] keyValue = Regex.Split(rawFile[i], ": ");
+			if (keyValue.Length == 2)
+			{
+				keyValuePairs.Add(keyValue);
+			}
+		}
+		for (int i = 0; i < keyValuePairs.Count; i++)
+		{
+			switch (keyValuePairs[i][0])
+			{
+				case "Time":
+				{
+					ext.time = float.Parse(keyValuePairs[i][1]);
+					break;
+				}
+				case "DayCyclePalettes":
+				{
+					string[] split = keyValuePairs[i][1].Split(';');
+					for (int j = 0; j < split.Length; j++)
+					{
+						string[] keyValue = split[j].Split(',');
+						if (keyValue[0].StartsWith("main_"))
+						{
+							ext.dayCyclePalettes[0, int.Parse(keyValue[0][5].ToString())] = int.Parse(keyValue[1]);
+						}
+					}
+					break;
+				}
+			}
+		}
+
+		return true;
 	}
 
 	private static void RoomSettingsPageOnctor(RoomSettingsPage.orig_ctor orig, DevInterface.RoomSettingsPage self, DevUI owner, string id, DevUINode parentNode, string name)
@@ -43,7 +133,7 @@ public class DayCycleHooks
 		
 		/*
 		for (int x = 0; x < 32; x++)
-		{
+		{t
 			for (int y = 0; y < 16; y++)
 			{
 				self.paletteTexture.SetPixel(x, y, self.paletteTexture.GetPixel(x, y) * 0.999f);
